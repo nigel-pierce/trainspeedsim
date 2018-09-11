59a01630d285c2863d178514b1f05dd44587a751
nigel <nodosaurus.nigel.p@gmail.com>
2018-09-11 12:50:40 -0700

    target & max speeds needed to be in f/s internally

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def sim_speed(maxspeeds, seg_len, accel):
     route_len = 5280*int(maxspeeds[-1].milepost - maxspeeds[0].milepost)
     position = 0 # in ft
     for segment_point in range(0, route_len, seg_len):
-        print("okisugiruuuu")
+    #    print("okisugiruuuu")
         sim_segment(bestspeeds, maxspeeds, int(segment_point/seg_len), seg_len, accel)
     
     return bestspeeds
@@ -45,22 +45,27 @@ def sim_segment(bestspeeds, maxspeeds, index, seg_len, accel):
         len_from_prev = seg_len
         
     v_init = bestspeeds[index-1]
-    #print(type(v_init), v_init)
-    v_max = lowest_current_max_speed(maxspeeds, index, seg_len, accel)
-    print(v_max)
+    print("for index", index, "v_init is", v_init)
+    v_max = lowest_current_max_speed(maxspeeds, index, seg_len, accel) * 5280/3600
+    #print(v_max)
     #v_next = lowest_upcoming_max_speed(maxspeeds, index, seg_len, accel)
     v_next = 1000000 # whatever will decelerate later
 
-    v_target = min(v_max, v_next)
+    v_target = min(v_max, v_next) * 5280 / 3600 # needs to be in fps
 
-    bestspeeds.append(min(v_max, accel_to_target(v_target, accel, v_init, seg_len * 5280)))
+    unbounded_v = accel_to_target(v_target, accel, v_init, seg_len)
+    print("for index", index, "unbounded speed is", unbounded_v, 'f/s')
+    bounded_v = min(v_max, unbounded_v)
+    print("for index", index, "bounded speed is  ", bounded_v, "f/s")
+    bestspeeds.append(bounded_v)
 
 
 # takes all units in feet units
 def accel_to_target(v_target, acc, v_i, d):
     #if v_target >= v_i: # which it SHOULD be? nah don't be conditional
         from math import sqrt
-        t = ( -v_i + sqrt(v_i**2 - 4 * (1/2) * acc * -d) ) / (2*(1/2)*acc)
+        t = ( -v_i + sqrt(v_i**2 - 4.0 * 0.5 * acc * -d) ) / (2.0*0.5*acc)
+        print("sec from prev index point segment guy:", t)
         v_f = acc * t + v_i
         return v_f
 
