1ac07aaf68ea508826a3a30b994c341b9d65959c
nigel <nodosaurus.nigel.p@gmail.com>
2021-08-09 18:16:17 -0700

    print shorttrack after the boundary shifting test

--- editablesim.py
+++ editablesim.py
@@ -921,6 +921,10 @@ class TestEditableTrack(unittest.TestCase):
                 Pos('0.1', 'mi').to_sm())
         self.assertEqual(self.shorttrack._track[0].length(), 0)
 
+        print("Short track after a bit of boundary shifting:")
+        print(self.shorttrack)
+        print("------")
+
 if __name__ == "__main__":
     seg = EditableTrackSeg(3, Pos('0', "mi").to_smaller_unit(), \
             Pos('0', "mi").to_smaller_unit(), Speed('0', 
