633fd6b6a8d3671870eb1a9819524b94423ec099
nigel <nodosaurus.nigel.p@gmail.com>
2021-08-28 20:44:35 -0700

    replacing TempTableController with TableController per diagram (with tweaks)

--- tableview.py
+++ tableview.py
@@ -8,6 +8,7 @@ from editablesim import EditableTrack
 from editablesim import SituationError, AmbiguousBoundaryError, \
     Adjacent0LenExistsError, Adjacent0LenPotentialError, \
     Non0LengthOf0SpeedSegPotentialError
+from observer import Observer
 
 class ViewFrame(tk.Frame):
     """the widget and elements and stuff"""
@@ -172,6 +173,39 @@ class TableView:
         self.frame.make_boundary_entries([ps.pos for ps in speed_limits])
         self.frame.make_limit_entries([ps.speed for ps in speed_limits])
         # do best_speeds later
+
+class TableController(Observer):
+    def __init__(self, model, parent_frame):
+        self._model = model
+        self._view = TableView(self, parent_frame)
+        self._update_view()
+
+    def notify(self, observable, message, arg):
+        if message not in ("ChangeSuccess", "ChangeFail", "NoChange"):
+            raise ValueError("Message "+str(message)+" is not known")
+
+        if observable is self._model:
+            if message == "ChangeSuccess":
+                print("Change success")
+                self._update_view()
+            elif message == "ChangeFail":
+                # arg should be an exception
+                print("Error: "+str(arg.args))
+                # update view anyway (easiest way to set correct view)
+                self._update_view()
+            elif message == "NoChange":
+                print("No change")
+            else:
+                # shouldn't get here
+                raise RuntimeError("should not get here (message and arg: "\
+                        "'{}'; '{}')".format(message, arg))
+        else:
+            raise ValueError("{} is not the model ({})".format(observable,
+                self._model))
+
+    def _update_view(self):
+        self._view.update([], self._model.get_limits())
+
 
 class TempTableController:
     '''Quick & dirty controller that owns model (EditableTrack) and view.
