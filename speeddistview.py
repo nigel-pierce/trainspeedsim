9520a513294b495009d1a7aa8c51ec9d5ec5bc47
nigel <nodosaurus.nigel.p@gmail.com>
2021-09-05 21:12:11 -0700

    beginning of SpeedDistViewFrame and its constructor

--- speeddistview.py
+++ speeddistview.py
@@ -5,3 +5,15 @@ import tkinter.ttk as ttk
 from simulation import PosSpeed
 from editablesim import EditableTrack
 from observer import Observer
+
+class SpeedDistViewFrame(tk.Frame):
+    '''Frame containing the canvas and whatever I decide to use for the
+    graph elements'''
+
+    def __init__(self, master, controller):
+        super().__init__(master)
+        self.master = master
+        self._controller = controller
+        # all that stuff should be in a more generic ViewFrame class
+        # (not to be confused with the tableview.py ViewFrame)
+
